be293c9f3ff55b2beedc710aa309df04ba2bb357
MAKOLAB-NET\mateusz.guzewski <mateusz.guzewski@makolab.com>
2017-03-07 15:15:06 +0100

    Add mateuszFunction method.

--- noname-project/src/main/java/pl/blank/noname_project/App.java
+++ noname-project/src/main/java/pl/blank/noname_project/App.java
@@ -4,10 +4,14 @@ package pl.blank.noname_project;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+
+    private String mateuszFunction(String value) {
+        return "Hello: " + value;
     }
 }
