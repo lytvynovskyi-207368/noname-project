04b67901f781205586cc9a4f7da60fae0d9ffb4a
MAKOLAB-NET\mateusz.guzewski <mateusz.guzewski@makolab.com>
2017-03-07 15:08:59 +0100

    Add empty maven project.

---
+++ noname-project/src/main/java/pl/blank/noname_project/App.java
@@ -0,0 +1,13 @@
+package pl.blank.noname_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
